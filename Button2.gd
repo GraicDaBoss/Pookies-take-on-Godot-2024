9204920d8adf173e381072959770f71d2573fff0
isabellamcks <145370220+isabellamcks@users.noreply.github.com>
2024-04-20 13:21:13 +0100

    kalimba
    kalimba button makes sound but magic wont for some reason :?

---
+++ Button2.gd
@@ -0,0 +1,5 @@
+extends Button
+
+func _on_button_mouse_entered():
+	$magic.play()
+	pass # Replace with function body.
---
+++ izzyscene.gd
@@ -0,0 +1,7 @@
+extends Node2D
+
+
+func _on_button_mouse_entered():
+	$Hitsound.play()
+	pass # Replace with function body.
+	
