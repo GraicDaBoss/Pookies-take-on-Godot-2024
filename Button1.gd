5eed9cf1e087bf3de5264274b4056607790e4ace
DanniHeart <145370320+DanniHeart@users.noreply.github.com>
2024-04-23 18:23:43 +0100

    Added sounds to buttons on my scene

--- Button1.gd
+++ Button1.gd
@@ -9,3 +9,4 @@ func _on_mouse_entered():
 	var pitch = basePitch + (0.1 * get_position().x) # Adjust the factor to get desired pitch variation
 	$Hitsound.pitch_scale = pitch
 	$Hitsound.play()
+
---
+++ DannisScene.gd
@@ -0,0 +1,53 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_button_pressed():
+	$Soundboom.play()
+	pass # Replace with function body.
+
+
+func _on_button_2_pressed():
+	$Soundboom2.play()
+	pass # Replace with function body.
+	
+	
+
+func _on_button_3_pressed():
+	$Soundboom3.play()
+	pass # Replace with function body.
+
+
+func _on_button_4_pressed():
+	$Soundboom4.play()
+	pass # Replace with function body.
+
+
+func _on_button_5_pressed():
+	$Soundboom7.play()
+	pass # Replace with function body.
+
+
+func _on_button_6_pressed():
+	$Soundboom5.play()
+	pass # Replace with function body.
+	
+
+
+func _on_button_7_pressed():
+	$Soundboom6.play()
+	pass # Replace with function body.
+
+
+func _on_button_8_pressed():
+	$Soundboom.play()
+	pass # Replace with function body.
