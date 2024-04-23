843e9c1a899d16702fff65a886937e0184d24746
isabellamcks <145370220+isabellamcks@users.noreply.github.com>
2024-04-23 11:26:20 +0100

    soundboard working
    kalimba sounds so whimsy :>

--- izzyscene.gd
+++ izzyscene.gd
@@ -11,3 +11,23 @@ func _on_button_mouse_entered():
 func _on_button_2_pressed():
 	$magic.play()
 	pass # Replace with function body.
+
+
+func _on_button_3_pressed():
+	$scale.play()
+	pass # Replace with function body.
+
+
+func _on_button_4_pressed():
+	$plastic.play()
+	pass # Replace with function body.
+
+
+func _on_button_5_pressed():
+	$whimsy.play()
+	pass # Replace with function body.
+
+
+func _on_button_6_pressed():
+	$sleepy.play()
+	pass # Replace with function body.
