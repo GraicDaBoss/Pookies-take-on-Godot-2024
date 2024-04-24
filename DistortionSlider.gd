68a72b5a0c4f986a31c36fb9e0fec32387c19567
GraicDaBoss <145370588+GraicDaBoss@users.noreply.github.com>
2024-04-24 13:19:51 +0100

    checkbox that makes it all fuzzy!!!!
    yeahhhhh

--- DistortionSlider.gd
+++ DistortionSlider.gd
@@ -1,5 +1,5 @@
 extends Node
-#grace
+#grace and sarah
 
 var distortion : AudioEffectDistortion
 
@@ -7,25 +7,30 @@ func _ready():
 	distortion = preload("res://default_bus_layout.tres::AudioEffectDistortion_ebiay")
 
 func _on_toggled(toggled_on):
-	# Get the index of the master bus
+	
 	var bus_index = AudioServer.get_bus_index("Master")
 
-	# Get the distortion effect on the master bus
-	var distortion_effect = AudioServer.get_bus_effect(bus_index, 1) # Assuming the distortion effect is the first effect on the master bus
+	
+	var distortion_effect = AudioServer.get_bus_effect(bus_index, 1) 
 
 	if distortion_effect:
+<<<<<<< Updated upstream
 		# Free the existing distortion effect instance
 		#distortion_effect.free()
+=======
+>>>>>>> Stashed changes
 		
-		# If the CheckBox is checked, add the distortion effect with mode set to MODE_OVERDRIVE
+
+
 		if toggled_on:
 			distortion.mode = AudioEffectDistortion.MODE_OVERDRIVE
+			distortion.pre_gain = 25.0
 		else:
 			distortion.mode = AudioEffectDistortion.MODE_CLIP
+			distortion.pre_gain = 0.0
+
 
 		# Add the updated distortion effect to the master bus
 		AudioServer.add_bus_effect(bus_index, distortion)
 	else:
 		print("Distortion effect not found on the master bus.")
-
-
