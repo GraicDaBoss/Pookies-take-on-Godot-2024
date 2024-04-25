c4ad05b0c2a178150a6005e3b7a7cbabcafa110b
emilyepc <145370205+emilyepc@users.noreply.github.com>
2024-04-25 01:46:29 +0100

    lamp assets + importing sound

---
+++ EmilysThings/birdsound.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+#func _on_button_pressed():
+	
+
+
+
+
+
+func _on_button_mouse_entered():
+	$birds.play() # Replace with function body.
